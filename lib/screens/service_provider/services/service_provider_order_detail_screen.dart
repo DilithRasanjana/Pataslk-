c05e85d7de4b19c58a6b102f44e7c2530d35ba55
costa-tech <w2084796@westminster.ac.uk>
2025-03-17 14:18:44 +0530

    Add imports for Firestore, FirebaseAuth, and CachedNetworkImage in order detail screen

--- lib/screens/service_provider/services/service_provider_order_detail_screen.dart
+++ lib/screens/service_provider/services/service_provider_order_detail_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import '../home/service_provider_home_screen.dart';
 
 class ServiceProviderOrderDetailScreen extends StatelessWidget {
