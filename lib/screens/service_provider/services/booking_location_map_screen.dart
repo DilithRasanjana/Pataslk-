d3c1e442cc91b2039d43873a452768a224692218
Thisun-17 <thisun.20233173@iit.ac.lk>
2025-03-21 11:19:26 +0530

    feat: enhance BookingLocationMapScreen with additional parameters for booking details

--- lib/screens/service_provider/services/booking_location_map_screen.dart
+++ lib/screens/service_provider/services/booking_location_map_screen.dart
@@ -2,3 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+class BookingLocationMapScreen extends StatefulWidget {
+  final LatLng location;
+  final String address;
+  final String bookingId;
+  final String customerName;
+
+  const BookingLocationMapScreen({
+    super.key,
+    required this.location, 
+    required this.address,
+    required this.bookingId,
+    this.customerName = "Customer",
+  });
